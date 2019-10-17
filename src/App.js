831d982f4d82ff091fdf4c91f6fc6bbbd130ac07
Jordan Miller <jordan.j.miller@live.com>
2019-10-17 16:47:17 -0700

    Mvps and some stretch complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
+import Forms from './Components/Forms.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <p>Add new Member</p>
+      <Forms />
     </div>
   );
 }
